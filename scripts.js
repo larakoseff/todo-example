ec235dce78a4fccb7cf57b129570e2f6d5446dec
larakoseff <larakoseff@gmail.com>
2023-10-14 10:23:02 +0200

    Example added

--- scripts.js
+++ scripts.js
@@ -1,28 +1,26 @@
 // @ts-check
 
 /* The Single-responsibility principle: "There should never be more than one
-* reason for a class to change." In other words, every class should have only
-* one resonsibility. 
-*/
+ * reason for a class to change." In other words, every class should have only
+ * one resonsibility.
+ */
 
 /* The Open-closed principle: "Software entities hsould be open for extension,
  * but closed for modification."
-*/
+ */
 
 /* The Liskov substitution principle: " Functions that use pointers or
  * references to base classes must be able to use objects of derived classes
- * without knowing it." See also design by contract. 
-*/
+ * without knowing it." See also design by contract.
+ */
 
 /* The Interface segregation principle: "Clienst should not be forced to depend
  * upon interfaces that they do not use."
-*/
+ */
 
 /* The Dependency inversion principle: "depend upon abstractions, [not]
  * concretions."
-*/
-
-
+ */
 
 // const list = [
 //     createTask({
@@ -53,17 +51,20 @@
 
 // list[1].completed = true;
 
+import { createTask } from "./modules/tasks.js";
+import { createAdding } from "./modules/adding.js";
 
-import { createTask } from "./modules/tasks.js"
-import { createAdding } from "./modules/adding.js"
+window.addEventListener("error", () => {
+    document.body.innerHTML = `<div>Something went very wrong.</div>`
+})
 
-const adding = createAdding()
+const adding = createAdding();
+adding.submission = createTask;
 
-adding.submission = createTask
+const example = createTask({
+    title: "Do homework",
+    due: null,
+    urgency: "high"
+})
 
-
-
-
-
-
-
+example.title = "w;eouqgfpo    wf"
